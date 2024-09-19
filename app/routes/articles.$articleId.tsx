c35accf5f60b53d9e54b57cd367fa162ed1bf932
Masahiko Kobayashi <kobaryubi6@gmail.com>
2024-09-19 18:27:12 +0900

    feat: handling

--- app/routes/articles.$articleId.tsx
+++ app/routes/articles.$articleId.tsx
@@ -2,18 +2,29 @@ import { json, type LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import invariant from "tiny-invariant";
 import { ArticleUseCases } from "~/application/usecases/ArticleUseCases";
+import { EntityNotFoundError } from "~/domain/errors/EntityNotFoundError";
 import { MockArticleRepository } from "~/infrastructure/repositories/MockArticleRepository";
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
   invariant(params.articleId, "Missing articleId param");
 
+  const articleId = parseInt(params.articleId, 10);
+  if (isNaN(articleId)) {
+    throw new Response("Invalid article ID", { status: 400 });
+  }
+
   const articleRepository = new MockArticleRepository();
   const useCases = new ArticleUseCases(articleRepository);
 
-  const articleId = parseInt(params.articleId, 10);
-  const article = await useCases.getArticle(articleId);
+  try {
+    const article = await useCases.getArticle(articleId);
 
-  return json({ article });
+    return json({ article });
+  } catch (error) {
+    if (error instanceof EntityNotFoundError) {
+      throw new Response("Not Found", { status: 404 });
+    }
+  }
 }
 
 export default function Article() {
